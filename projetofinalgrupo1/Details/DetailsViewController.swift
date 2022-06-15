8a67704c4c6718b6a94d5d58b3f8a4df92fefc0e
mairatalma <mahtalma@hotmail.com>
2022-06-15 18:46:41 -0300

    tela de detalhes

--- projetofinalgrupo1/Details/DetailsViewController.swift
+++ projetofinalgrupo1/Details/DetailsViewController.swift
@@ -2,17 +2,30 @@
 //  DetailsViewController.swift
 //  projetofinalgrupo1
 //
-//  Created by Bruno Alves on 15/06/22.
+//  Created by Máira Talma on 15/06/22.
 //
 
 import UIKit
 
 class DetailsViewController: UIViewController {
-
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var animalLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationItem.title = "Detalhes"
+    
+        image.layer.cornerRadius = 8
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.tintColor = .systemBlue
+        navigationController?.navigationBar.titleTextAttributes = [
+            .foregroundColor: UIColor.systemBlue
+        ]
+    }
 }
