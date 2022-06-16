9bf51faf579cb2974e92a29de0b38c9a99848161
mairatalma <mahtalma@hotmail.com>
2022-06-16 13:31:48 -0300

    adicionei animalModel para details

--- projetofinalgrupo1/Details/DetailsViewController.swift
+++ projetofinalgrupo1/Details/DetailsViewController.swift
@@ -9,16 +9,29 @@ import UIKit
 
 class DetailsViewController: UIViewController {
     
-    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var animalLabel: UILabel!
+    @IBOutlet weak var specieLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    
+    private let animalModel: AnimalModel
+    
+    init(animalModel: AnimalModel) {
+        self.animalModel = animalModel
+        super.init(nibName: nil, bundle: nil)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Detalhes"
-    
-        image.layer.cornerRadius = 8
+        setupData()
+        imageView.layer.cornerRadius = 8
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -27,5 +40,12 @@ class DetailsViewController: UIViewController {
         navigationController?.navigationBar.titleTextAttributes = [
             .foregroundColor: UIColor.systemBlue
         ]
+    }
+    
+    private func setupData(){
+        imageView.image = animalModel.image
+        nameLabel.text = animalModel.name
+        specieLabel.text = animalModel.specie
+        descriptionLabel.text = animalModel.description
     }
 }
---
+++ projetofinalgrupo1/Models/AnimalModel.swift
@@ -0,0 +1,15 @@
+//
+//  AnimalModel.swift
+//  projetofinalgrupo1
+//
+//  Created by Máira Talma on 16/06/22.
+//
+
+import UIKit
+
+struct AnimalModel {
+    let image: UIImage?
+    let name: String
+    let specie: String
+    let description: String 
+}
