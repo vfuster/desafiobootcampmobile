a5fb4f7e4f070ac64c1e13d736c14ce65f64de1e
Valeria Fuster <vfustermartins@gmail.com>
2022-06-18 14:44:37 -0300

    create register view

--- projetofinalgrupo1/TabBarViewController.swift
+++ projetofinalgrupo1/TabBarViewController.swift
@@ -8,39 +8,39 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setViewControllers([homeController(),registerController(),favoriteController()], animated: true)
         selectedIndex = 0
         setupTabBar()
     }
     
-        private func setupTabBar(){
+    private func setupTabBar(){
         tabBar.backgroundColor = .cyan
         tabBar.barTintColor = .black
         tabBar.isTranslucent = false
     }
-  
+    
     private func homeController()  -> UINavigationController {
-    let HomeController = HomeViewController()
-    HomeController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName:"house.fill"), tag: 0)
-    let navigationController = UINavigationController(rootViewController: HomeController)
-    return navigationController
+        let HomeController = HomeViewController()
+        HomeController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName:"house.fill"), tag: 0)
+        let navigationController = UINavigationController(rootViewController: HomeController)
+        return navigationController
     }
     
     private func registerController()  -> UINavigationController {
-    let registerController = RegisterViewController()
-    registerController.tabBarItem = UITabBarItem(title: "Cadastrar", image: UIImage(systemName:"plus.circle"), tag: 0)
-    let navigationController = UINavigationController(rootViewController: registerController)
-    return navigationController
+        let registerController = RegisterViewController(nibName: "RegisterViewController", bundle: nil)
+        registerController.tabBarItem = UITabBarItem(title: "Cadastrar", image: UIImage(systemName:"plus.circle"), tag: 0)
+        let navigationController = UINavigationController(rootViewController: registerController)
+        return navigationController
     }
     
     private func favoriteController()  -> UINavigationController {
-    let favoriteController = FavoriteViewController()
-    favoriteController.tabBarItem = UITabBarItem(title: "Favoritos", image: UIImage(systemName:"star.fill"), tag: 0)
-    let navigationController = UINavigationController(rootViewController: favoriteController)
-    return navigationController
+        let favoriteController = FavoriteViewController()
+        favoriteController.tabBarItem = UITabBarItem(title: "Favoritos", image: UIImage(systemName:"star.fill"), tag: 0)
+        let navigationController = UINavigationController(rootViewController: favoriteController)
+        return navigationController
     }
- }
+}
