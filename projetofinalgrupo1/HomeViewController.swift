05830c509004593c3a7eb35e1bcdcb34d62c113a
mcznh <mariacarolina.mf@gmail.com>
2022-06-19 12:58:14 -0300

    setup tableView constraints

--- projetofinalgrupo1/HomeViewController.swift
+++ projetofinalgrupo1/HomeViewController.swift
@@ -8,24 +8,36 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    
+    private let tableView = UITableView()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .white
-
-        // Do any additional setup after loading the view.
+        setupView()
+        setupTableView()
+        
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func setupView(){
+        title = "Home"
+        
     }
-    */
+    
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.backgroundColor = .red
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+    }
+    
+    
+    
+    
 
 }
