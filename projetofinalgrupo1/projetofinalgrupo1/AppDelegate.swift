0fc00b7d31dc74a8a9684d9da3654b655cf02c79
mcznh <mariacarolina.mf@gmail.com>
2022-06-19 13:43:48 -0300

    setup project for Xib use

--- projetofinalgrupo1/projetofinalgrupo1/AppDelegate.swift
+++ projetofinalgrupo1/projetofinalgrupo1/AppDelegate.swift
@@ -9,16 +9,16 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-var window: UIWindow?
-
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let viewController = TabBarViewController()
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.rootViewController = viewController
-       
+        
         return true
     }
-
+    
 }
